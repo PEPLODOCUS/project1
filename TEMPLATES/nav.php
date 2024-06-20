52e3c69e37b2565f438da3eba1f3171f8b48ca97
PEPLODOCUS <kihuridavid1@gmail.com>
2024-06-20 12:23:10 +0300

    added the templates folder in the project

---
+++ TEMPLATES/nav.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="toplink">
+        <a href="./">Home</a>
+        <a href="about.php">About Us</a>
+        <a href="projects.php">Our Projects</a>
+        <a href="products.php">Products</a>
+        <a href="contacts.php">Contact Us</a>
+        <div class="toplink-right">
+            <a href="signup.php">Sign Up</a>
+            <a href="signin.php">Sign In</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ TEMPLATES/sidebar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="side_bar">
+        <h2>Short Stories from Mombasa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates optio ex fugiat exercitationem velit? Odio aspernatur maiores ducimus ipsa fugit optio amet nobis, officiis pariatur ab nihil omnis quibusdam repellendus?</p>
+        <ul>
+            <li>HTML</li>
+            <li>Java</li>
+            <li>JavaScript</li>
+            <li>MySQL</li>
+            <li>PHP</li>
+        </ul>
+        <ol type="I" start="4">
+            <li>black</li>
+            <li>red</li>
+            <li>yellow</li>
+            <li>pink</li>
+            <li>white</li>
+        </ol>
+    </div>
+</body>
+</html>
